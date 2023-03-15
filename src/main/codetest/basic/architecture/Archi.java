6a3706ded3d0254d8cc302f7cc1d9d28c6ef3277
yjkim <qlrogo91lp@gmail.com>
2023-03-15 13:31:57 +0900

    문제007 주몽의 명령
    - 투 포인터
      - class 파일 제거
      - java 파일 추가

--- src/main/codetest/basic/architecture/Archi.java
+++ src/main/codetest/basic/architecture/Archi.java
@@ -1,9 +1,48 @@
 package main.codetest.basic.architecture;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Archi {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World !!");
+    public static void main(String[] args) throws Exception {
+        Archi exe = new Archi();
+        exe.no007();
+
+    }
+
+    // 문제 007 주몽의 명령 - 제한 시간 2초, 난이도 실버4, 백준 1940번
+    private void no007() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        int count = 0;
+        int i = 0;
+        int j = N - 1;
+
+        while (i < j) {
+            if (A[i] + A[j] < M) {
+                i++;
+            } else if (A[i] + A[j] > M) {
+                j--;
+            } else {
+                count++;
+                i++;
+                j--;
+            }
+        }
+        System.out.println(count);
+        bf.close();
     }
 
 }
